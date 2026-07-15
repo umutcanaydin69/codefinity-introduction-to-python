12b449db3dac072aa8f8141864f02d3a069009d8
umutcanaydin69 <>
2026-07-15 09:22:28 +0000

    Combined Conditions

---
+++ conditional_statements/combined_conditions/main.py
@@ -0,0 +1,7 @@
+# The item's discount and stock status have been defined
+discounted = False
+lowStock = True
+movingProduct = discounted or lowStock
+promotion = not discounted and not lowStock
+
+print("Is the item eligible for promotion?", promotion)
