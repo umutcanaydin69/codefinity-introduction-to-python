c268add76bf500d724148ba37fffb0a1ecea2f52
umutcanaydin69 <>
2026-07-20 17:35:26 +0000

    Challenge: Sales and Shipments

---
+++ loops/challenge_sales_and_shipments/main.py
@@ -0,0 +1,22 @@
+# List of products with their initial stock levels at the start of the week
+products = [
+    ["Apples", 150],  
+    ["Bananas", 200],
+    ["Oranges", 100],
+    ["Mangoes", 120]
+]
+
+# List of products sold by the end of the week
+units_sold = [["Apples", 30], ["Bananas", 45], ["Oranges", 20], ["Mangoes", 10]]
+
+# New shipment received at the end of the week
+shipment_received = [["Apples", 50], ["Bananas", 70], ["Oranges", 30], ["Mangoes", 40]]
+
+for index in range(len(products)):
+    products[index][1] -= units_sold[index][1]
+
+for i in range(len(products)):
+    products[i][1] += shipment_received[i][1]
+
+print(f"Final stock levels for all products: {products}")
+    
